092ef37854b34fe1a6f70df7bd15c6f696188023
troj4n <abhinabamitra19@gmail.com>
2021-11-22 13:46:12 +0530

    Basic Learning
    Developed basic understanding of modules

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+//const firstname = require('./utils.js')
+const addition = require('./utils.js')
+
+const sum = addition(12,-8)
+
+//const name = "Abhinaba"
+
+console.log(sum)
+*/
+
+const getNotes = require('./notes.js')
+const op = getNotes()
+
+console.log(op)
