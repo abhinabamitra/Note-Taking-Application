836726db6b22edc0f40e17a31b32df16fea97572
troj4n <abhinabamitra19@gmail.com>
2021-11-28 22:13:54 +0530

    Update app.js
    Studied how to use Nodemon

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = chalk.bold.grey
+const msg = chalk.strikethrough.bold.red
 
 const op = getNotes()
 console.log(msg(op))
 
-console.log(msg(validator.isEmail('abhinabamitra19@gmail.com')))
+console.log(validator.isEmail('abhinabamitra19@gmail.com'))
