3f7500a84a846f95424267a1d3f7352537babbb3
troj4n <abhinabamitra19@gmail.com>
2021-11-30 20:38:58 +0530

    Update app.js
    Watched 4.2 command line arguments

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const validator = require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = chalk.strikethrough.bold.red
+/*const msg = chalk.strikethrough.bold.red
 
 const op = getNotes()
 console.log(msg(op))
 
 console.log(validator.isEmail('abhinabamitra19@gmail.com'))
+*/
+
+const command = process.argv[2]
+
+console.log(command)
